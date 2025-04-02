a3441081bb1b3e393b31aeb128904520ba2587d3
Ivan <tw40210@gmail.com>
2025-04-02 20:28:56 +0100

    feat: ui beautify

--- src/py_libs/qa_gpt/script/QA_ui.py
+++ src/py_libs/qa_gpt/script/QA_ui.py
@@ -3,36 +3,16 @@ import os
 
 import streamlit as st
 
+from src.py_libs.qa_gpt.core.utils.display_utils import (
+    display_question,
+    display_summary_beautifully,
+    get_parsed_question,
+)
+
 
 def load_json_from_file(file_path):
     with open(file_path) as file:
         return json.load(file)
-
-
-def get_parsed_question(question_object) -> dict:
-    parsed_question = {}
-    choices = ["choice_1", "choice_2", "choice_3", "choice_4"]
-    parsed_question["question_description"] = question_object["question_description"]
-    parsed_question["options"] = [
-        question_object[choice]["choice_description"] for choice in choices
-    ]
-    parsed_question["answers"] = [
-        idx for idx, choice in enumerate(choices) if question_object[choice]["answer"]
-    ]
-    parsed_question["explanation"] = [question_object[choice]["explanation"] for choice in choices]
-
-    return parsed_question
-
-
-def display_question(question, options, question_key):
-    st.write(question)
-    selected_options = []
-
-    for idx, option in enumerate(options):
-        if st.checkbox(option, key=f"{question_key}_{option}"):
-            selected_options.append(idx)
-
-    return selected_options
 
 
 # Set wide mode
@@ -114,7 +94,7 @@ with col2:
             if selected_summary:
                 summary_path = os.path.join(material_folder_path, selected_summary)
                 summary = load_json_from_file(summary_path)
-                st.json(json.dumps(summary, indent=4))
+                display_summary_beautifully(summary, "Material Summary")
         else:
             st.write("No summary files found for this material.")
     else:
